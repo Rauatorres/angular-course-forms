f29b3b505d51bb9fb523e86347c51aa82467f797
rauatorres <rauatorres@gmail.com>
2026-09-09 17:49:04 -0300

    lendo inputs do formulário

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(formData: NgForm) {
+    const email = formData.form.value.email;
+    const password = formData.form.value.password;
+    console.log(email, password);
+  }
+}
